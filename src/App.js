948428dc6f8e67b54bbec7d5a97efa4c994f0478
Nicholasmcosta <nicholasmcosta@gmail.com>
2022-08-04 15:33:32 -0400

    pls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import React, {useState,useEffect} from "react"
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import cellEditFactory from "react-bootstrap-table2-editor"
+import filterFactory, {textFilter} from "react-bootstrap-table2-filter";
 
+ 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    getData()
+  }, []);
+
+  const getData=()=>{
+    axios.get('data.json')
+    .then((res) => {
+      console.log(res.data);
+      setData(res.data);
+
+    });
+  };
+
+  
+  const emailFormatter =(data,row) => {
+    return <span> {data}</span>
+  }
+  const columns=[
+    {
+      dataField:"first_name",
+      text:"First Name",
+      sort: true,
+      formatter: emailFormatter,
+      filter: textFilter()
+    },
+    {
+      dataField:"last_name",
+      text:"Last Name",
+      sort: true,
+      formatter: emailFormatter,
+      filter: textFilter()
+    },
+    {
+      dataField: "salary",
+      text: "Salary",
+      sort: true,
+      validator: (newValue, row, column) => {
+        if (isNaN(newValue)) {
+          return{
+            valid:false,
+            message:"Please enter a numeric value"
+          }
+        }
+        return true
+      },
+      filter: textFilter()
+    },
+    {
+      dataField: "age",
+      text: "Age",
+      sort: true,
+      validator: (newValue, row, column) => {
+        if (isNaN(newValue)) {
+          return{
+            valid:false,
+            message:"Please enter a numeric value"
+          }
+        }
+        return true
+      },
+      filter: textFilter()
+    },
+    {
+      dataField:"address",
+      text:"Address",
+      sort: true,
+      filter: textFilter()
+    },
+
+  ];
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <BootstrapTable 
+      keyField="id" data={data} 
+      columns={columns} 
+      striped 
+      hover 
+      condensed 
+      pagination={paginationFactory()}
+      cellEdit={ cellEditFactory({ 
+        mode: 'dbclick',
+        blurToSave: true, 
+      }) }
+      filter= {filterFactory()}
+     />
+  </div>
   );
 }
 
+ 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
